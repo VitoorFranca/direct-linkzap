4807314b8d360c6d5adb7753dfab964494e67398
Vitor França <franc.vitoor@gmail.com>
2021-03-21 23:20:57 -0300

    Add ads

--- pages/head/Head.js
+++ pages/head/Head.js
@@ -8,6 +8,7 @@ export default ({ title }) => {
             <link rel="icon" href="/favicon.ico" />
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" integrity="sha256-46r060N2LrChLLb5zowXQ72/iKKNiw/lAmygmHExk/o=" crossorigin="anonymous" />
             <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet' />
+            <script data-ad-client="ca-pub-1534821690624998" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
     )
 }
